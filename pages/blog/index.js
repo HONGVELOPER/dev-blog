c76bf064fa03280b62839c880e0aa96c5c1a6855
Young-Jin1003 <hyjinn1003@naver.com>
2021-09-04 17:02:32 +0900

    error check

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,24 +1,14 @@
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '../../components/index/Header.js'
 import Container from '../../components/blog/Container.js';
 import Footer from '../../components/index/Footer.js'
 import axios from 'axios';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: '#F1F1F1',
-    height: '100vw'
-  },
-}))
-
 const Blog = ({ data }) => {
-  
-  const classes = useStyles()
 
   return (
     <div>
       <AppBar />
-      <Container data={data} />
+      {/* <Container data={data} /> */}
       <Footer style={{backgroundColor: "white"}} />
     </div>
   )
@@ -26,13 +16,13 @@ const Blog = ({ data }) => {
 
 export default Blog
 
-export async function getServerSideProps() {
-  console.log('ssr rendering start')
-  const response = await axios.get('http://localhost:3000/api/blog')
-  // console.log(response.data, 'response check')
-  return {
-    props: {
-      data: response.data,
-    }
-  }
-}
+// export async function getServerSideProps() {
+//   console.log('ssr rendering start')
+//   const response = await axios.get('http://localhost:3000/api/blog')
+//   // console.log(response.data, 'response check')
+//   return {
+//     props: {
+//       data: response.data,
+//     }
+//   }
+// }
