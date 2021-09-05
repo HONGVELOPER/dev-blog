ee8e8ae8368817a0fba3b340576fb16766506bdd
Young-Jin1003 <hyjinn1003@naver.com>
2021-09-05 14:52:53 +0900

    path error check

--- components/blogDetail/Container.js
+++ components/blogDetail/Container.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import BreadCrumbs from '../breadCrumbs.js';
+import BreadCrumbs from '../index/breadCrumbs.js';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
