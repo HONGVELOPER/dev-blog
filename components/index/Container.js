7dd7958f97a7cf2b00586f3e8a66b159fef19e65
HONGVELOPER <hyjinn1003@naver.com>
2022-04-01 19:40:20 +0900

    header width modify and skill add.

--- components/index/Container.js
+++ components/index/Container.js
@@ -1,105 +1,152 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Image from 'next/image';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
+import Image from "next/image";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 750,
-      md: 870,
-      lg: 1280,
-      xl: 1920
-    }
-  }
-})
+	breakpoints: {
+		values: {
+			xs: 0,
+			sm: 750,
+			md: 870,
+			lg: 1280,
+			xl: 1920,
+		},
+	},
+});
 
 const useStyles = makeStyles((theme) => ({
 	root: {
-    	padding: 10
+		padding: 10,
+		position: "relative",
+		left: "20px",
 	},
 	indexText: {
-		fontSize: '42px',
+		fontSize: "42px",
 		fontWeight: 600,
-		paddingTop: '12vh',
-		paddingBottom: '11vh',
-		paddingLeft: '6vw',
-		minHeight: '520px',
+		paddingTop: "12vh",
+		paddingBottom: "11vh",
+		paddingLeft: "7vw",
+		minHeight: "520px",
 	},
 	mobileIndexText: {
-		fontSize: '6vw',
+		fontSize: "6vw",
 		fontWeight: 600,
-		paddingTop: '5vh',
-		paddingBottom: '2vh',
-		paddingLeft: '5vw',
+		paddingTop: "5vh",
+		paddingBottom: "2vh",
+		paddingLeft: "5vw",
 	},
 	indexImage: {
-		position: 'relative',
-		maxWidth: '450px',
+		position: "relative",
+		maxWidth: "450px",
 		padding: 30,
-		bottom: '20px',
+		bottom: "20px",
 	},
-}))
+}));
 
 const IndexContainer = () => {
-
-	const classes = useStyles()
-	const [mobile, setMobile] = useState(null)
-	const divide = 870
+	const classes = useStyles();
+	const [mobile, setMobile] = useState(null);
+	const divide = 870;
 
 	useEffect(() => {
 		if (mobile === null) {
-			window.innerWidth < divide ? setMobile(true) : setMobile(false)
+			window.innerWidth < divide ? setMobile(true) : setMobile(false);
 		} else {
-			window.addEventListener('resize', function() {
-				window.innerWidth < divide ? setMobile(true) : setMobile(false)
-			}, {passive: true})
+			window.addEventListener(
+				"resize",
+				function () {
+					window.innerWidth < divide
+						? setMobile(true)
+						: setMobile(false);
+				},
+				{ passive: true }
+			);
 		}
-	})
+	});
 
-  	return (
-    <>
-		<Container className={classes.root}>
-			<Grid container>
-				<MuiThemeProvider theme={theme}>
-					<Grid item xs={12} md={5}>
-						{mobile ? (
-							<div className={classes.mobileIndexText}>
-								웹 브라우저로<br />
-								생각을 표현하는 개발자<br />
-								<span style={{color: '#218e16'}}>홍영진</span>입니다.
-								<div style={{fontSize: '3vw', marginTop: '20px'}}>
-									성장하는 개발자가 될 것입니다.
+	return (
+		<>
+			<Container className={classes.root}>
+				<Grid container>
+					<MuiThemeProvider theme={theme}>
+						<Grid item xs={12} md={5}>
+							{mobile ? (
+								<div className={classes.mobileIndexText}>
+									안녕하세요,
+									<br />
+									코드를 통해
+									<br />
+									생각을 표현하는 개발자
+									<br />
+									<span style={{ color: "#218e16" }}>
+										홍영진
+									</span>
+									입니다.
+									<div
+										style={{
+											fontSize: "3vw",
+											marginTop: "20px",
+										}}
+									>
+										성장하는 개발자가 될 것입니다.
+									</div>
 								</div>
-							</div>
-						) : (
-							<div className={classes.indexText}>
-								안녕하세요,<br />
-								웹 브라우저로 생각을<br />
-								표현하는<br />
-								개발자 <span style={{color: '#218e16'}}>홍영진</span>입니다.
-								<div style={{fontSize: '20px', marginTop: '20px'}}>
-									성장하는 개발자가 될 것입니다.
+							) : (
+								<div className={classes.indexText}>
+									안녕하세요,
+									<br />
+									코드를 통해 생각을
+									<br />
+									표현하는
+									<br />
+									개발자{" "}
+									<span style={{ color: "#218e16" }}>
+										홍영진
+									</span>
+									입니다.
+									<div
+										style={{
+											fontSize: "20px",
+											marginTop: "20px",
+										}}
+									>
+										성장하는 개발자가 될 것입니다.
+									</div>
 								</div>
-							</div>
-						)}
-					</Grid>
-					<Grid item xs={1} />
-					<Grid item xs={12} md={6} className={classes.indexImage}>
-						{mobile ? (
-							<Image src={'/images/con4.png'} width={100} height={80} layout="responsive" quality={60} />
-						) : (
-							<Image src={'/images/con4.png'} layout="fill" objectFit="contain" quality={60} />
-						)}
-					</Grid>
-				</MuiThemeProvider>
-			</Grid>
-		</Container>
-    </>
-  	)
-}
+							)}
+						</Grid>
+						<Grid item xs={1} />
+						<Grid
+							item
+							xs={12}
+							md={6}
+							className={classes.indexImage}
+						>
+							{mobile ? (
+								<Image
+									src={"/images/mainIamge.png"}
+									width={100}
+									height={80}
+									layout="responsive"
+									quality={60}
+								/>
+							) : (
+								<Image
+									src={"/images/mainIamge.png"}
+									layout="fill"
+									objectFit="contain"
+									quality={60}
+								/>
+							)}
+						</Grid>
+					</MuiThemeProvider>
+				</Grid>
+			</Container>
+		</>
+	);
+};
 
-export default IndexContainer
+export default IndexContainer;
--- components/index/Skill.js
+++ components/index/Skill.js
@@ -1,143 +1,207 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Image from 'next/image'
-import Card from '@material-ui/core/Card';
-import Typography from '@material-ui/core/Typography';
-import CardContent from '@material-ui/core/CardContent';
-import Divider from '@material-ui/core/Divider';
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Image from "next/image";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import CardContent from "@material-ui/core/CardContent";
+import Divider from "@material-ui/core/Divider";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
-		marginTop: '3vw',
-		fontSize: '1.5vw',
+		marginTop: "3vw",
+		fontSize: "1.5vw",
 	},
-  	skill: {
-		fontSize: '32px',
-		textAlign: 'center',
-		'&::after': {
+	skill: {
+		fontSize: "32px",
+		textAlign: "center",
+		"&::after": {
 			content: "''",
-			display: 'block',
-			width: '60px',
-			borderBottom: '3px solid #218e16',
-			margin: '10px auto'
-		}
+			display: "block",
+			width: "60px",
+			borderBottom: "3px solid #218e16",
+			margin: "10px auto",
+		},
 	},
 	card: {
 		borderRadius: 10,
-		boxShadow: '3px 3px 3px #3c5c5e',
-		display: 'inline-block',
-		marginLeft: '10px',
-		marginBottom: '10px',
-		width: '6.5vw',
-		minWidth: '73px',
+		boxShadow: "3px 3px 3px #3c5c5e",
+		display: "inline-block",
+		marginLeft: "10px",
+		marginBottom: "10px",
+		width: "6.5vw",
+		minWidth: "73px",
 	},
 	skillDesc: {
-		fontSize: '25px',
-		marginTop: '2vh'
-	}
-}))
+		fontSize: "25px",
+		marginTop: "2vh",
+	},
+}));
 
 const Skill = (props) => {
-	const classes = useStyles()
-	const [mobile, setMobile] = useState(null)
-	const divide = 800
+	const classes = useStyles();
+	const [mobile, setMobile] = useState(null);
+	const divide = 800;
 
 	useEffect(() => {
 		if (mobile === null) {
-			window.innerWidth < divide ? setMobile(true) : setMobile(false)
+			window.innerWidth < divide ? setMobile(true) : setMobile(false);
 		}
-			window.addEventListener('resize', function() {
-			window.innerWidth < divide ? setMobile(true) : setMobile(false)
-		}, {passive: true})
-	})
+		window.addEventListener(
+			"resize",
+			function () {
+				window.innerWidth < divide ? setMobile(true) : setMobile(false);
+			},
+			{ passive: true }
+		);
+	});
 
 	useEffect(() => {
 		if (props.toSkill) {
-			document.querySelector('.skill').scrollIntoView({behavior: 'smooth'})
-			props.scrollChange(false)
+			document
+				.querySelector(".skill")
+				.scrollIntoView({ behavior: "smooth" });
+			props.scrollChange(false);
 		}
-	})
+	});
 
 	const frontEnd = [
-		'javascript',
-		'html',
-		'css',
-		'react',
-		'next',
-		'material-ui',
-		'vue',
-		'nuxt',
-		'vuetify',
-	]
+		"javascript",
+		"html",
+		"css",
+		"react",
+		"next",
+		"material-ui",
+		"vue",
+		"nuxt",
+		"vuetify",
+	];
 
 	const backEnd = [
-		'node',
-		'express',
-		'mysql',
-		'sequelize'
-	]
+		"node",
+		"express",
+		"mysql",
+		"sequelize",
+		"nest",
+		"graphql",
+		"postgresql",
+	];
 
-	const devOps = [
-		'aws',
-		'docker',
-		'git',
-		'github'
-	]
+	const devOps = ["aws", "docker", "git", "github"];
 
 	const frontEndList = frontEnd.map((name) => (
-		<Card key={name} variant="outlined" elevation={10} className={classes.card}>
-			<CardContent style={{padding: 0}}>
-				<Image src={`/images/frontend/${name}.svg`} width={100} height={100} layout="responsive" />
+		<Card
+			key={name}
+			variant="outlined"
+			elevation={10}
+			className={classes.card}
+		>
+			<CardContent style={{ padding: 0 }}>
+				<Image
+					src={`/images/frontend/${name}.svg`}
+					width={100}
+					height={100}
+					layout="responsive"
+				/>
 			</CardContent>
 			<Divider variant="middle" />
 		</Card>
-	))
+	));
 
 	const backEndList = backEnd.map((name) => (
-		<Card key={name} variant="outlined" elevation={10} className={classes.card}>
-			<CardContent style={{padding: 0}}>
-				<Image src={`/images/backend/${name}.svg`} width={100} height={100} layout="responsive" />
+		<Card
+			key={name}
+			variant="outlined"
+			elevation={10}
+			className={classes.card}
+		>
+			<CardContent style={{ padding: 0 }}>
+				<Image
+					src={`/images/backend/${name}.svg`}
+					width={100}
+					height={100}
+					layout="responsive"
+				/>
 			</CardContent>
 			<Divider variant="middle" />
 		</Card>
-	))
+	));
 
 	const devOpsList = devOps.map((name) => (
-		<Card key={name} variant="outlined" elevation={10} className={classes.card}>
-			<CardContent style={{padding: 0}}>
-				<Image src={`/images/devops/${name}.svg`} width={100} height={100} layout="responsive" />
+		<Card
+			key={name}
+			variant="outlined"
+			elevation={10}
+			className={classes.card}
+		>
+			<CardContent style={{ padding: 0 }}>
+				<Image
+					src={`/images/devops/${name}.svg`}
+					width={100}
+					height={100}
+					layout="responsive"
+				/>
 			</CardContent>
 			<Divider variant="middle" />
 		</Card>
-	))
+	));
 
-  	return (
+	return (
 		<>
-			<Container>
+			<Container style={{ maxWidth: "1100px" }}>
 				<section className={classes.root}>
-					<div className='skill' style={{color: '#218e16', fontSize: '29px', fontWeight: 600}}>Skills</div>
-					<div style={{fontSize: '15px', paddingTop: '1vw', fontWeight: 50}}>
+					<div
+						className="skill"
+						style={{
+							color: "#218e16",
+							fontSize: "29px",
+							fontWeight: 600,
+						}}
+					>
+						Skills
+					</div>
+					<div
+						style={{
+							fontSize: "15px",
+							paddingTop: "1vw",
+							fontWeight: 50,
+						}}
+					>
 						웹 개발을 하며 사용해본 기술 스택입니다.
 					</div>
-					<Grid container spacing={5} style={{marginTop: 30}}>
+					<Grid container spacing={5} style={{ marginTop: 30 }}>
 						<Grid item xs={12}>
 							<Grid container>
 								<Grid item xs={12}>
-									<Typography variant="h6" border={1} gutterBottom className={classes.skillDesc}>
+									<Typography
+										variant="h6"
+										border={1}
+										gutterBottom
+										className={classes.skillDesc}
+									>
 										Front-End
 									</Typography>
 									{frontEndList}
 								</Grid>
 								<Grid item xs={12}>
-									<Typography variant="h6" border={1} gutterBottom className={classes.skillDesc}>
+									<Typography
+										variant="h6"
+										border={1}
+										gutterBottom
+										className={classes.skillDesc}
+									>
 										Back-End
 									</Typography>
 									{backEndList}
 								</Grid>
 								<Grid item xs={12}>
-									<Typography variant="h6" border={1} gutterBottom className={classes.skillDesc}>
+									<Typography
+										variant="h6"
+										border={1}
+										gutterBottom
+										className={classes.skillDesc}
+									>
 										DevOps
 									</Typography>
 									{devOpsList}
@@ -146,10 +210,10 @@ const Skill = (props) => {
 						</Grid>
 					</Grid>
 				</section>
-				<Divider style={{marginTop: '50px'}} />
+				<Divider style={{ marginTop: "50px" }} />
 			</Container>
 		</>
-  	)
-}
+	);
+};
 
-export default Skill
+export default Skill;
